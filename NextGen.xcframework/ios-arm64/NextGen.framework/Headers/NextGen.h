2d9b77bed864f8bab4abc0f947c2fa588d71b34e
anubhavpulkit <anubhavssingh177@gmail.com>
2024-03-04 15:35:28 +0530

    first commit

---
+++ NextGen.xcframework/ios-arm64/NextGen.framework/Headers/NextGen.h
@@ -0,0 +1,18 @@
+//
+//  NextGen.h
+//  NextGen
+//
+//  Created by Anubhav Singh on 03/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NextGen.
+FOUNDATION_EXPORT double NextGenVersionNumber;
+
+//! Project version string for NextGen.
+FOUNDATION_EXPORT const unsigned char NextGenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NextGen/PublicHeader.h>
+
+
